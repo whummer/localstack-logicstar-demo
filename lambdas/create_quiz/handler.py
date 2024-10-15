e2bc6567d19c107e544d78f727bf28774c873721
Harsh Mishra <erbeusgriffincasper@gmail.com>
2024-10-15 22:52:42 +0530

    make quiz id easy to use

--- lambdas/create_quiz/handler.py
+++ lambdas/create_quiz/handler.py
@@ -1,6 +1,6 @@
 import json
 import boto3
-import uuid
+import random
 
 def lambda_handler(event, context):
     try:
@@ -10,19 +10,25 @@ def lambda_handler(event, context):
     except (KeyError, json.JSONDecodeError) as e:
         return {
             'statusCode': 400,
-            'body': json.dumps({'message': 'Invalid input data', 'error': str(e)})
+            'body': json.dumps({
+                'message': 'Invalid input data',
+                'error': str(e)
+            })
         }
 
     for question in questions:
         if not all(k in question for k in ('QuestionText', 'Options', 'CorrectAnswer', 'Trivia')):
             return {
                 'statusCode': 400,
-                'body': json.dumps({'message': 'Each question must contain QuestionText, Options, CorrectAnswer, and Trivia'})
+                'body': json.dumps({
+                    'message': 'Each question must contain QuestionText, Options, CorrectAnswer, and Trivia'
+                })
             }
 
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('Quizzes')
-    quiz_id = str(uuid.uuid4())
+
+    quiz_id = str(random.randint(100000, 999999))
     quiz_data['QuizID'] = quiz_id
 
     table.put_item(Item=quiz_data)
--- tests/run.sh
+++ tests/run.sh
@@ -22,7 +22,7 @@ curl -X POST "$API_ENDPOINT/createquiz" \
 
 # Get the quiz; Change the ID below
 
-curl -X GET "$API_ENDPOINT/getquiz?quiz_id=8d641539-9608-4d19-95f1-861e91dc9f4b"
+curl -X GET "$API_ENDPOINT/getquiz?quiz_id=195167"
 
 # Submit responses
 
@@ -30,7 +30,7 @@ curl -X POST "$API_ENDPOINT/submitquiz" \
 -H "Content-Type: application/json" \
 -d '{
     "Username": "user1",
-    "QuizID": "8d641539-9608-4d19-95f1-861e91dc9f4b",
+    "QuizID": "195167",
     "Answers": {
         "0": "D. Paris",
         "1": "B. Shakespeare"
@@ -43,7 +43,7 @@ curl -X POST "$API_ENDPOINT/submitquiz" \
 -d '{
     "Username": "user2",
     "Email": "user@example.com",
-    "QuizID": "8d641539-9608-4d19-95f1-861e91dc9f4b",
+    "QuizID": "195167",
     "Answers": {
         "0": "D. Paris",
         "1": "B. Shakespeare"
@@ -54,7 +54,7 @@ curl -X POST "$API_ENDPOINT/submitquiz" \
 -H "Content-Type: application/json" \
 -d '{
     "Username": "user3",
-    "QuizID": "8d641539-9608-4d19-95f1-861e91dc9f4b",
+    "QuizID": "195167",
     "Answers": {
         "0": "D. Paris",
         "1": "D. Hemingway"
@@ -67,7 +67,7 @@ curl -X GET "$API_ENDPOINT/getsubmission?submission_id=ed6f3128-20c3-45ce-847f-6
 
 # Get leaderboard
 
-curl -X GET "$API_ENDPOINT/getleaderboard?quiz_id=8d641539-9608-4d19-95f1-861e91dc9f4b&top=3"
+curl -X GET "$API_ENDPOINT/getleaderboard?quiz_id=195167&top=3"
 
 # Check SES
 curl -s http://localhost.localstack.cloud:4566/_aws/ses
